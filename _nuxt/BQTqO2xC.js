590aa9d7ae6ac41a8f0ffaf48c570c3d758ac9ec
Achraf931 <45672435+Achraf931@users.noreply.github.com>
2025-05-22 12:46:37 +0000

    Deploying to gh-pages from @ Achraf931/landing@e0b68e03c87e96d38799d3f29385593e662f964d 🚀

---
+++ _nuxt/BQTqO2xC.js
@@ -0,0 +1,1 @@
+import{bS as e,r as o}from"./PdQD6mYu.js";function n(){return{open:o(!1)}}const a=e(n);export{a as u};
---
+++ _nuxt/Vlx-DUn0.js
@@ -0,0 +1,1 @@
+import{r as s}from"./CtWlL9Cu.js";import e from"./BvO8ZzEX.js";import{z as t,B as a,e as p,w as c,E as n}from"./PdQD6mYu.js";import"./C2bHYaEW.js";const d={__name:"ProseTip",setup(i){const o=t();return(r,f)=>(p(),a(e,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
